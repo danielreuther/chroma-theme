c192bb0a80fc4600408a558734314899e7d65172
Daniel Reuther <daniel.reuther@liferay.com>
2014-05-22 00:03:50 +0200

    First working version

--- docroot/_diffs/js/main.js
+++ docroot/_diffs/js/main.js
@@ -1,6 +1,18 @@
 AUI().ready(
 	'aui-carousel',
 	function(A) {
+		var navigation = A.one('#navigation');
+
+		if (navigation) {
+			navigation.plug(Liferay.NavigationInteraction);
+		}
+
+		var siteBreadcrumbs = A.one('#breadcrumbs');
+
+		if (siteBreadcrumbs) {
+			siteBreadcrumbs.plug(A.Hudcrumbs);
+		}
+
 		var mainCarousel = A.one("#main-carousel");
 
 		if (mainCarousel) {
